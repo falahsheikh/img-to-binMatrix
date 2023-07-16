30e2308da7f5d165633cc292e41b6ef15df51946
falahsheikh <112839415+falahsheikh@users.noreply.github.com>
2023-07-16 00:27:34 -0600

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("org.jpeg")
+img = cv2.imread("org.jpeg") #replace the image file you want to process
 WIDTH, HEIGHT, _ = img.shape
 
-cell_width, cell_height = 5, 5  # Decreased cell size for more detail
+cell_width, cell_height = 5, 5  
 new_width, new_height = int(WIDTH / cell_width), int(HEIGHT / cell_height)
 
 img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_NEAREST)
@@ -12,7 +12,7 @@ img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_NEAREST)
 new_img = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
 
 font_path = cv2.FONT_HERSHEY_SIMPLEX
-font_scale = 0.3  # Decreased font scale for smaller characters
+font_scale = 0.3  
 font_thickness = 1
 
 for i in range(new_height):
@@ -28,4 +28,4 @@ for i in range(new_height):
         cv2.putText(new_img, text, text_origin, font_path, font_scale, (0, int(g), 0), font_thickness, cv2.LINE_AA)
 
 # Save the image using OpenCV
-cv2.imwrite("matrix.png", new_img)
+cv2.imwrite("matrix.png", new_img) #the processed file will output as 'matrix.png'
